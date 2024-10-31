600a009b76539b7929aef32095b81692375b87de
kageds <alan@kageds.com>
2024-10-31 15:55:31 +0000

    upgrade erlang.mk before make and then you wont need my hack

--- make/deps.mk
+++ make/deps.mk
@@ -64,9 +64,9 @@ dep_amqp_client = hex 3.7.8
 dep_certifi = hex 0.3.0
 dep_chatterbox = hex 0.7.0
 dep_eflame = git https://github.com/slfritchie/eflame 7b0bb1a7e8c8482a59421a3a50ae69d49af59d52
-dep_eiconv = git https://github.com/kageds/eiconv master-OTP24
+dep_eiconv = git https://github.com/zotonic/eiconv
 dep_escalus = git https://github.com/esl/escalus 0de0463c345a1ade6fccfb9aadad719b58a1cef5
-dep_exml = git https://github.com/kageds/exml 2.2.1-OTP24
+dep_exml = git https://github.com/paulgray/exml 2.2.1
 dep_jiffy = git https://github.com/kageds/erlang-jiffy 2600hz-OTP24
 #dep_jiffy = git https://github.com/lazedo/jiffy utf8  ## utf8 decode
 dep_meck = git https://github.com/eproxus/meck
@@ -113,7 +113,7 @@ dep_horse = git https://github.com/ninenines/horse
 
 dep_proper = git https://github.com/manopapad/proper v1.2
 
-dep_syslog = git https://github.com/kageds/erlang-syslog bbad537-OTP24
+dep_syslog = git https://github.com/2600hz/erlang-syslog bbad537a1cb5e4f37e672d2e2665659e850662d0
 
 dep_fcm = git https://github.com/softwarejoint/fcm-erlang.git b2f68a4c6f0f59475597a35e2dc9be13d9ba2910
 
